f1cd1307cd899a43734c7ff7adff393c722d16c0
Lindsay Grizzard <lindsaygrizzard@cheetos.local>
2015-06-09 16:04:10 -0700

    demo ready

--- server.py
+++ server.py
@@ -398,7 +398,7 @@ def process_logout():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
     connect_to_db(app)
